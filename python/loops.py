a169506156d746213142373822200d0119d8c798
lucas.correa <contato.lucasecorrea@gmail.com>
2023-01-22 18:14:23 -0300

    lecture python

---
+++ python/loops.py
@@ -0,0 +1,7 @@
+# for i in range(6):
+#     print(i)
+
+name = "Harry"
+
+for character in name:
+    print(character)
---
+++ python/sequences.py
@@ -0,0 +1,9 @@
+name = ["Harry","John","Maria"]
+print(name[0])
+
+coordinateX = 10.0
+coordinateY = 20.0
+
+coordinate = (10.0, 20.0)
+
+print(coordinate[1])
